9482968e18dbe222f77eadceb019e35de920cd36
jaeyjeon <n8720432@naver.com>
2024-01-24 03:57:05 +0900

    add naver map

--- parc1/src/app/layout.tsx
+++ parc1/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import './globals.css'
 import styles from "../styles/baseLayout.module.css"
 import Foot from '../components/footer'
+import Map from '../components/map'
 
 export default function RootLayout({
   children,
@@ -22,13 +23,20 @@ export default function RootLayout({
 
   return (
       <html lang="en">
+        <head>
+          <script
+            type="text/javascript"
+            src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_MAP_KEY}`}
+            async
+          /> {/* https://nextjs.org/docs/messages/no-sync-scripts */}
+        </head>
         <body>
           <section className={styles.container}>
             <aside className={styles.sidebar}>
               사이드바
             </aside>
             <section className={styles.map}>
-              지도지도지도
+              <Map />
             </section>
             <div className={styles.maincontent} style={{width:FlexbarWidth}}>
               <main>
--- parc1/src/components/map.tsx
+++ parc1/src/components/map.tsx
@@ -1,6 +1,29 @@
+import { useEffect, useRef } from "react";
 
 const MapNaverDefault = () => {
 
+  const mapElement = useRef(null);
+  const { naver } = window;
+
+  useEffect(() => {
+    if (!mapElement.current || !naver) return;
+    // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
+
+    var mapOptions = {
+      center: new naver.maps.LatLng(37.5070856701229, 127.062897340803),
+      zoom: 18,
+      zoomControl: true,
+	  mapTypeControl: true
+    };
+
+    var map = new naver.maps.Map(mapElement.current, mapOptions);
+  }, []);
+
+  return (
+    <>
+      <div ref={mapElement} style={{ minHeight: "100vh" }} />
+    </>
+  );
 };
 
 export default MapNaverDefault;
